5fbe615d74a5bb5f4192e7b541398a2796490722
brawny271 <ashishpardeshi9967@gmail.com>
2024-07-28 13:38:58 +0530

    Banner active

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Banner.css';
 
 const Banner = () => {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const images = [
+    'https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg',
+    'https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg',
+    'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg'
+  ];  
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="banner">
       <div className="banner-content">
         <div className="banner-image">
-          <img src="banner_image_url" alt="Watches" />
+          <img src={images[currentImageIndex]} alt="Watches" />
         </div>
         <div className="banner-text">
           <h1>The Great Luxury Watch Sale</h1>
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,9 +1,13 @@
-import React, { useEffect } from 'react';
-import axios from 'axios';
-import ProductCard from './ProductCard';
-import './ProductList.css';
-import { setProducts, setProductsError, setFilterOptions } from '../Redux/productSlice';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useEffect } from "react";
+import axios from "axios";
+import ProductCard from "./ProductCard";
+import "./ProductList.css";
+import {
+  setProducts,
+  setProductsError,
+  setFilterOptions,
+} from "../Redux/productSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 const ProductList = () => {
   const dispatch = useDispatch();
@@ -12,31 +16,41 @@ const ProductList = () => {
   const filters = useSelector((state) => state.data.filters);
 
   useEffect(() => {
-    axios.get('https://fakestoreapi.com/products')
-      .then(response => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((response) => {
         dispatch(setProducts(response.data));
       })
-      .catch(error => {
+      .catch((error) => {
         console.error(error);
         dispatch(setProductsError(error.message));
       });
   }, [dispatch]);
 
   const filterProducts = (products, filters, selectedCategory) => {
-    const { category: categoryFilters = [], priceRange: priceRangeFilters = [], ratingRange: ratingRangeFilters = [] } = filters || {};
+    const {
+      category: categoryFilters = [],
+      priceRange: priceRangeFilters = [],
+      ratingRange: ratingRangeFilters = [],
+    } = filters || {};
 
-    return products.filter(product => {
-      const categoryMatch = selectedCategory === 'all' || product.category === selectedCategory;
-      
-      const categoryFilterMatch = categoryFilters.length === 0 || categoryFilters.includes(product.category);
-      
-      const priceMatch = priceRangeFilters.length === 0 || priceRangeFilters.some(range =>
-        product.price >= range.min && product.price <= range.max
-      );
-      
-      const ratingMatch = ratingRangeFilters.length === 0 || ratingRangeFilters.some(range =>
-        product.rating.rate >= range.min
-      );
+    return products.filter((product) => {
+      const categoryMatch =
+        selectedCategory === "all" || product.category === selectedCategory;
+
+      const categoryFilterMatch =
+        categoryFilters.length === 0 ||
+        categoryFilters.includes(product.category);
+
+      const priceMatch =
+        priceRangeFilters.length === 0 ||
+        priceRangeFilters.some(
+          (range) => product.price >= range.min && product.price <= range.max
+        );
+
+      const ratingMatch =
+        ratingRangeFilters.length === 0 ||
+        ratingRangeFilters.some((range) => product.rating.rate >= range.min);
 
       return categoryMatch && categoryFilterMatch && priceMatch && ratingMatch;
     });
@@ -46,8 +60,23 @@ const ProductList = () => {
 
   return (
     <div className="product-list">
-      {filteredProducts.map(product => (
-        <ProductCard key={product.id} product={product} />
+      {filteredProducts.map((product, index) => (
+        <React.Fragment key={product.id}>
+          <ProductCard product={product} />
+          {index === 0 && (
+            <div className="ad-card">
+              <h3>Sponsored</h3>
+              <p>This is an advertisement</p>
+              <a
+                href="https://www.google.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Visit Google
+              </a>
+            </div>
+          )}
+        </React.Fragment>
       ))}
     </div>
   );
